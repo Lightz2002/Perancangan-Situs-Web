f26f5bee72f14a7d972347b30ec383ec1390fe32
Lightz2002 <kenidyryan@gmail.com>
2021-04-24 15:38:34 +0700

    perbaiki services

--- javascript/script.js
+++ javascript/script.js
@@ -5,24 +5,34 @@ const webDesignDesc = description[1];
 const webDevelopDesc = description[2];
 const webDesign = document.querySelector(".web-design");
 const webDevelop = document.querySelector(".web-develop");
-
-// description[0].style.background = "Red";
+let width = screen.width;
 
 window.addEventListener("load", function () {
   webHostDesc.style.transform = "translate(0)";
+  console.log(width);
+  if (width <= 992) {
+    webDesignDesc.style.transform = "translate(0)";
+  }
 });
 
 window.addEventListener("scroll", function () {
   var wScroll = window.scrollY;
+  let WebDevContainerPos = getPos(webDevelop).y;
+  let WebDesContainerPos = getPos(webDesign).y;
 
-  if (wScroll >= 300) {
+  if (wScroll >= WebDesContainerPos - 400) {
     webDesign.style.transform = "translateY(0)";
     webDesign.style.opacity = "1";
     webDesignDesc.style.transform = "translate(0)";
   }
 
-  if (wScroll >= 1100) {
+  if (wScroll >= WebDevContainerPos - 400) {
     webDevelop.style.transform = "scale(1)";
     webDevelopDesc.style.transform = "scale(1)";
   }
 });
+
+function getPos(el) {
+  for (var lx = 0, ly = 0; el != null; lx += el.offsetLeft, ly += el.offsetTop, el = el.offsetParent);
+  return { x: lx, y: ly };
+}
